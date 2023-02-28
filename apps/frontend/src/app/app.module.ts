ae62fd1d6ac6115723e0f6432484b274172dd11c
Hugo Mendes <hugo.mendes@noreply.github.com>
2023-02-28 17:36:03 +0100

    Add empty front with eslint/stylelint/prettier

---
+++ apps/frontend/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+	bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [AppRoutingModule, BrowserModule],
+	providers: []
+})
+export class AppModule {}
