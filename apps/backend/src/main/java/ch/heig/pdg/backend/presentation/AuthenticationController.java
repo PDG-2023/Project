8d101a7cda48d993f8d5ed916faae60a4d9616dc
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2023-05-15 14:21:26 +0200

    Auth routes

---
+++ apps/backend/src/main/java/ch/heig/pdg/backend/presentation/AuthenticationController.java
@@ -0,0 +1,8 @@
+package ch.heig.pdg.backend.presentation;
+
+import ch.heig.pdg.backend.api.JwtApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController implements JwtApi {
+}
