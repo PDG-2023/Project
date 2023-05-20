91dd99417f6b378e2e3ec545e54cd44a4efb78ae
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-05-20 17:34:00 +0200

    [FIX] Materials path

--- apps/frontend/src/app/components/components.module.ts
+++ apps/frontend/src/app/components/components.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
-import { MaterialsModule } from "./materials.module";
+import { MaterialsModule } from "../materials/materials.module";
 import { TranslationModule } from "../translation";
 
 @NgModule({
