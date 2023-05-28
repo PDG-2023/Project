74efd47a2036bf171d058a8b43e823e26c785f2d
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2023-05-28 11:25:30 +0200

    [TASK] Added filter basics

--- apps/backend/src/main/java/ch/heig/pdg/backend/utils/HugoSearchFilter.java
+++ apps/backend/src/main/java/ch/heig/pdg/backend/utils/HugoSearchFilter.java
@@ -1,10 +1,20 @@
 package ch.heig.pdg.backend.utils;
 
+import jakarta.persistence.criteria.CriteriaQuery;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.util.Optional;
 
-public class HugoSearchFilter {
+public class HugoSearchFilter<T> {
+    public final CriteriaQuery<T> criteriaQuery;
+    public final Integer offset;
+    public final Integer limit;
+
+    private HugoSearchFilter(CriteriaQuery<T> criteriaQuery, Integer offset, Integer limit) {
+        this.criteriaQuery = criteriaQuery;
+        this.offset = offset;
+        this.limit = limit;
+    }
 
     public static HugoSearchFilter build(Optional<NativeWebRequest> request){
         if(request.isEmpty()){
@@ -13,7 +23,6 @@ public class HugoSearchFilter {
 
         NativeWebRequest req = request.get();
 
-//        req.get
-        return new HugoSearchFilter();
+        return new HugoSearchFilter(null, 0, 0);
     }
 }
