f11d4b29c0da6dc3fe3e3f11657668e3cc1102e7
Simon Ackermann <simon@inthebox.ch>
2023-05-28 20:27:14 +0200

    [FEATURE] Implement item-model service & api

--- apps/backend/src/main/java/ch/heig/pdg/backend/presentation/ItemModelController.java
+++ apps/backend/src/main/java/ch/heig/pdg/backend/presentation/ItemModelController.java
@@ -1,7 +1,64 @@
 package ch.heig.pdg.backend.presentation;
 
+import ch.heig.pdg.backend.dto.ItemModelDTO;
+import ch.heig.pdg.backend.entities.ItemModel;
+import ch.heig.pdg.backend.services.ItemModelService;
+import ch.heig.pdg.backend.utils.HugoSearchFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class ItemModelController implements ch.heig.pdg.backend.api.ItemModelApi {
+public class ItemModelController extends AbstractController implements ch.heig.pdg.backend.api.ItemModelApi {
+    private final ItemModelService itemModelService;
+
+    public ItemModelController(HttpServletRequest httpServletRequest, ItemModelService itemModelService) {
+        super(httpServletRequest);
+        this.itemModelService = itemModelService;
+    }
+
+    @Override
+    public ResponseEntity<ItemModelDTO> createItemModel(Integer inventoryId, ItemModelDTO itemModelDTO) {
+        return new ResponseEntity<>(
+                this.itemModelService.addItemModel(inventoryId, itemModelDTO),
+                HttpStatus.CREATED
+        );
+    }
+
+    @Override
+    public ResponseEntity<ItemModelDTO> deleteItemModel(Integer id) {
+        return new ResponseEntity<>(
+                this.itemModelService.removeItemModel(id),
+                HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<ItemModelDTO> getItemModel(Integer id) {
+        return new ResponseEntity<>(
+                this.itemModelService.getItemModel(id),
+                HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<List<ItemModelDTO>> getItemModels(Integer inventoryId) {
+        HugoSearchFilter<ItemModel> filter = HugoSearchFilter.build(this.httpServletRequest);
+
+        return new ResponseEntity<>(
+                this.itemModelService.getItemModels(inventoryId, filter),
+                HttpStatus.OK
+        );
+    }
+
+    @Override
+    public ResponseEntity<ItemModelDTO> updateItemModel(Integer id, ItemModelDTO itemModelDTO) {
+        return new ResponseEntity<>(
+                this.itemModelService.updateItemModel(id, itemModelDTO),
+                HttpStatus.OK
+        );
+    }
 }
--- apps/backend/src/main/java/ch/heig/pdg/backend/services/ItemModelService.java
+++ apps/backend/src/main/java/ch/heig/pdg/backend/services/ItemModelService.java
@@ -1,4 +1,72 @@
 package ch.heig.pdg.backend.services;
 
-public class ItemModelService {
+import ch.heig.pdg.backend.dto.ItemModelDTO;
+import ch.heig.pdg.backend.dto.mapping.ItemModelMapper;
+import ch.heig.pdg.backend.entities.Inventory;
+import ch.heig.pdg.backend.entities.ItemModel;
+import ch.heig.pdg.backend.repositories.InventoryRepository;
+import ch.heig.pdg.backend.repositories.ItemModelRepository;
+import ch.heig.pdg.backend.utils.HugoSearchFilter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ItemModelService extends AbstractService {
+    private final ItemModelRepository itemModelRepository;
+    private final ItemModelMapper itemModelMapper;
+
+    protected ItemModelService(InventoryRepository inventoryRepository, ItemModelRepository itemModelRepository, ItemModelMapper itemModelMapper) {
+        super(inventoryRepository);
+        this.itemModelRepository = itemModelRepository;
+        this.itemModelMapper = itemModelMapper;
+    }
+
+    public ItemModelDTO addItemModel(Integer inventoryId, ItemModelDTO itemModelDTO) {
+        Inventory inventory = this.checkInventory(inventoryId);
+
+        ItemModel itemModel = this.itemModelMapper.createFromDTO(itemModelDTO);
+        itemModel.setInventory(inventory);
+        // FIXME: register categories (need to add it to api yaml
+//        itemModel.setCategories();
+
+        return (ItemModelDTO) this.itemModelMapper.getDTO(
+                this.itemModelRepository.save(itemModel)
+        );
+    }
+
+    public ItemModelDTO removeItemModel(Integer id) {
+        ItemModel itemModel = this.getEntityIfExists(id, this.itemModelRepository);
+        this.itemModelRepository.deleteById(id);
+        return (ItemModelDTO) this.itemModelMapper.getDTO(itemModel);
+    }
+
+    public ItemModelDTO getItemModel(Integer id) {
+        return (ItemModelDTO) this.itemModelMapper.getDTO(
+                this.getEntityIfExists(id, this.itemModelRepository)
+        );
+    }
+
+    public List<ItemModelDTO> getItemModels(Integer inventoryId, HugoSearchFilter<ItemModel> filter) {
+        this.checkInventory(inventoryId);
+
+        return this.itemModelRepository
+                .findByFilter(filter)
+                .stream()
+                .map(i -> (ItemModelDTO) this.itemModelMapper.getDTO(i))
+                .collect(Collectors.toList());
+    }
+
+    public ItemModelDTO updateItemModel(Integer id, ItemModelDTO itemModelDTO) {
+        ItemModel itemModel = this.getEntityIfExists(id, this.itemModelRepository);
+        // FIXME: register categories (need to add it to api yaml
+//        itemModel.setCategories();
+
+        return (ItemModelDTO) this.itemModelMapper.getDTO(
+                this.itemModelRepository.save(
+                        this.itemModelMapper.updateFromDTO(itemModel, itemModelDTO)
+                )
+        );
+    }
 }
