ae51779be6eeebe57bad9a374837904523d52687
Simon Ackermann <simon@inthebox.ch>
2023-05-28 10:43:38 +0200

    Adapt api.yaml and add missing controller and service

---
+++ apps/backend/src/main/java/ch/heig/pdg/backend/presentation/CategoryController.java
@@ -0,0 +1,7 @@
+package ch.heig.pdg.backend.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements ch.heig.pdg.backend.api.CategoryApi {
+}
