fbe4f6b08c38d2d3ba1e4df6f17c0442deb97022
Simon Ackermann <simon@inthebox.ch>
2023-06-01 19:16:46 +0200

    [BUGFIX] Adapt to match checkstyle in style

--- apps/backend/src/test/java/ch/heig/pdg/backend/CategoryControllerIntegrationTest.java
+++ apps/backend/src/test/java/ch/heig/pdg/backend/CategoryControllerIntegrationTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
