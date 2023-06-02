ce844cc317fec6fce1a1ca148a1fc6f549632a74
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-06-02 11:59:23 +0200

    Set the webpack proxy (avoid CORS)

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -2,6 +2,7 @@ import { Environment } from "./environment.interface";
 
 export const environment: Environment = {
 	backend: {
-		url: "http://localhost:8080/api"
+		// Use the webpack proxy
+		url: `${window.location.origin}/api`
 	}
 };
