baac3b4ab87bf7ceab6a10e930547a86052a6eb4
Simon Ackermann <simon@inthebox.ch>
2023-06-04 00:47:52 +0200

    Add unit test for DateFormatUtils

---
+++ apps/backend/src/test/java/ch/heig/pdg/backend/unit/DateFormatUtilsTest.java
@@ -0,0 +1,23 @@
+package ch.heig.pdg.backend.unit;
+
+import ch.heig.pdg.backend.utils.DateFormatUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+
+public class DateFormatUtilsTest {
+
+    @Test
+    public void testDateToString() {
+        LocalDateTime date = LocalDateTime.of(2023, 6, 4, 15, 30);
+        String dateString = DateFormatUtil.dateToString(date);
+        Assertions.assertEquals("2023-06-04T15:30", dateString);
+    }
+
+    @Test
+    public void testStringToDate() {
+        String dateString = "2023-06-04T15:30";
+        LocalDateTime date = DateFormatUtil.stringToDate(dateString);
+        Assertions.assertEquals(LocalDateTime.of(2023, 6, 4, 15, 30), date);
+    }
+}
