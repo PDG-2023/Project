5d5907c18a4cddfc729b10aa1fa68017eb342f7a
Simon Ackermann <simon@inthebox.ch>
2023-06-05 10:04:50 +0200

    Directly use the JWTGeneratorService instead of authenticating the user

--- apps/backend/src/test/java/ch/heig/pdg/backend/utility/AbstractAuthenticatedIntegrationTest.java
+++ apps/backend/src/test/java/ch/heig/pdg/backend/utility/AbstractAuthenticatedIntegrationTest.java
@@ -1,41 +1,20 @@
 package ch.heig.pdg.backend.utility;
 
-import ch.heig.pdg.backend.dto.CredentialsDTO;
-import ch.heig.pdg.backend.dto.JWTDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import ch.heig.pdg.backend.security.services.JWTGeneratorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public abstract class AbstractAuthenticatedIntegrationTest {
     @Autowired
     protected MockMvc mvc;
+
+    @Autowired
+    private JWTGeneratorService jwtGeneratorService;
     protected String token = "";
 
     @BeforeEach
     public void authenticateUser() throws Exception {
-        CredentialsDTO credentials = new CredentialsDTO();
-        credentials.setPassword("password");
-        credentials.setUsername("ptest");
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-
-        MvcResult result = this.mvc.perform(post("/authentication/getToken")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(credentials))
-                )
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
-
-        String content = result.getResponse().getContentAsString();
-        JWTDTO jwt = mapper.readValue(content, JWTDTO.class);
-        this.token = jwt.getToken();
+        this.token = this.jwtGeneratorService.generateJWT(1);
     }
 }
